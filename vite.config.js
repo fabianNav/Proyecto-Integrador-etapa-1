093bae538fcbdd30fc07ac99493d9a8320ae375d
fabianNav <fn39001@gmail.com>
2023-08-03 11:21:37 -0300

    Creamos las otras paginas y las vinculamos

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
+import { resolve } from "node:path"
+
+console.log (resolve("pages"))
+
 export default {
     server: {
         port: "2222",
     },
     css: {
         devSourcemap: true,
+    },
+    build: {
+        emptyOutDir: true,
+        rolluOptions: {
+            input: {
+                carrito: resolve('pages/carrito.html'),
+                nosotros: resolve('pages/nosotros.html'),
+                contacto: resolve('pages/contacto.html'),
+                index: resolve('index.html')
+            }
+        }
     }
 }
