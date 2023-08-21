50ff55269cf911e9705034716025c5ea61f3e52e
fabianNav <fn39001@gmail.com>
2023-08-21 17:31:41 -0300

    tratamos de subir la pagina a netlify

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,14 @@ export default {
         emptyOutDir: true,
         rolluOptions: {
             input: {
-                carrito: resolve('pages/carrito.html'),
-                categorias: resolve('pages/categorias.html'),
+                accesorios: resolve('pages/accesorios.html'),
+                contectividad: resolve('pages/conectividad.html'),
+                consolas: resolve('pages/consolas.html'),
+                notebooks: resolve('pages/notebooks.html'),
+                perifericos: resolve('pages/consolas.html'),
+                carrito: resolve('pages/perifericos.html'),
+                software: resolve('pages/software.html'),
+                categorias: resolve('pages/todo.html'),
                 nosotros: resolve('pages/nosotros.html'),
                 contacto: resolve('pages/contacto.html'),
                 index: resolve('index.html')
